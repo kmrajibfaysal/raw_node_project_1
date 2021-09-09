88b36c5809e7dfff8a3220a1cc718b81b181dce7
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-09-09 15:07:02 +0600

    aaaa

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const faysal = 44;
-const rajib = 99;
+/**
+ * Title: Uptime Monitoring Application
+ * Author: K. M. Rajib Faysal
+ * Date: 09/09/2021
+ *
+ */
 
-console.log(faysal + rajib);
+// dependencies
+const http = require('http');
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`listening to port ${app.config.port}`);
+    });
+};
+
+// handle Request Response
+app.handleReqRes = (req, res) => {
+    // response handle
+    res.end('Hello programmers');
+};
+
+// start the server
+app.createServer();
